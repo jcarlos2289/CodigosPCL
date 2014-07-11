6ac29d56aa7497f4225c23f8f85f1f60880d593d
jcarlos2289 <rovit@rovit.(none)>
2014-07-11 17:44:01 +0200

    Codigos de Descriptores 3D

---
+++ 3DSC/build/CMakeFiles/3dsc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/3dsc.dir/3dsc.cpp.o"
+  "3dsc.pdb"
+  "3dsc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/3dsc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ FPFH_OMP/build/CMakeFiles/fpfh.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/fpfh.dir/fpfh.cpp.o"
+  "fpfh.pdb"
+  "fpfh"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/fpfh.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
