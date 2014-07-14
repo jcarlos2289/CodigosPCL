91c650406b72acd0bc20400439a40052aade900f
jcarlos2289 <rovit@rovit.(none)>
2014-07-14 12:12:48 +0200

    USC

---
+++ USC/build/CMakeFiles/usc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/usc.dir/usc.cpp.o"
+  "usc.pdb"
+  "usc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/usc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
