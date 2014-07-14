a215f3faa4129731ae4ec47355375869032282cb
jcarlos2289 <rovit@rovit.(none)>
2014-07-14 17:49:20 +0200

    RIFT

---
+++ RIFT/build/CMakeFiles/rift.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rift.dir/rift.cpp.o"
+  "rift.pdb"
+  "rift"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/rift.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
