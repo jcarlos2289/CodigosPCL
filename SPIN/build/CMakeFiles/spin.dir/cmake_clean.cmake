a4c9efafb53a37b75b50f18d947aa22fb1945333
jcarlos2289 <rovit@rovit.(none)>
2014-07-14 16:29:45 +0200

    Spin Image

---
+++ SPIN/build/CMakeFiles/spin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/spin.dir/spin.cpp.o"
+  "spin.pdb"
+  "spin"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/spin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
